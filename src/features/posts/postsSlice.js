7610af37c95b21c91b9ea49886b2a83215ca6710
Nico Rithner <nico@altvia.com>
2021-08-25 13:05:30 -0600

    Complete part 5: Async Logic and Data Fetching
    Co-authored-by: Nico nicorithner@gmail.com

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -13,6 +13,17 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   return response.posts
 })
 
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  // The payload creator receives the partial `{title, content, user}` object
+  async initialPost => {
+    // We send the initial data to the fake API server
+    const response = await client.post('/fakeApi/posts', { post: initialPost })
+    // The response includes the complete post object, including unique ID
+    return response.post
+  }
+)
+
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
@@ -22,22 +33,6 @@ const postsSlice = createSlice({
       const existingPost = state.posts.find(post => post.id === postId)
       if (existingPost) {
         existingPost.reactions[reaction]++
-      }
-    },
-    postAdded: {
-        reducer(state, action) {
-        state.posts.push(action.payload)
-      },
-      prepare(title, content, userId) {
-        return {
-          payload: {
-            id: nanoid(),
-            date: new Date().toISOString(),
-            title,
-            content,
-            user: userId
-          }
-        }
       }
     },
     postUpdated(state, action) {
@@ -61,6 +56,10 @@ const postsSlice = createSlice({
     [fetchPosts.rejected]: (state, action) => {
       state.status = 'failed'
       state.error = action.error.message
+    },
+    [addNewPost.fulfilled]: (state, action) => {
+      // We can directly add the new post object to our posts array
+      state.posts.push(action.payload)
     }
   }
 })

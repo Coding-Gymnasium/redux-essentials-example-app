0a6589052b78c489108d57091986fc04964d3aec
Nico Rithner <nico@altvia.com>
2021-08-25 12:43:25 -0600

    Load users
    Co-authored-by: Nico nicorithner@gmail.com

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -7,21 +7,8 @@ import TimeAgo from './TimeAgo';
 import { ReactionButtons } from './ReactionButtons';
 import { selectAllPosts, fetchPosts } from './postsSlice';
 
-export const PostsList = () => {
-  const dispatch = useDispatch();
-  const posts = useSelector(selectAllPosts);
-
-  const postStatus = useSelector(state => state.posts.status)
-
-  useEffect(() => {
-    if (postStatus === 'idle') {
-      dispatch(fetchPosts())
-    }
-  }, [postStatus, dispatch])
-
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-
-  const renderedPosts = orderedPosts.map(post => (
+const PostExcerpt = ({ post }) => {
+  return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <div>
@@ -35,12 +22,43 @@ export const PostsList = () => {
         View Post
       </Link>
     </article>
-  ))
+  )
+}
+
+export const PostsList = () => {
+  const dispatch = useDispatch();
+  const posts = useSelector(selectAllPosts);
+
+  const postStatus = useSelector(state => state.posts.status);
+  const error = useSelector(state => state.posts.error);
+
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postStatus, dispatch])
+
+  let content
+
+    if (postStatus === 'loading') {
+    content = <div className="loader">Loading...</div>
+  } else if (postStatus === 'succeeded') {
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map(post => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
